7be0f6edd47efca9277a1272a0bf1c47765a087c
Joe Fehrman <joseph.fehrman@gmail.com>
2024-04-29 22:05:07 -0400

    feat: Removing all JavaScript in favor of using the new popover api.

--- src/components/Component.mjs
+++
@@ -1,9 +0,0 @@
-export default class Component extends HTMLElement {
-    /**
-     * @param {any[]} args
-     */
-    /* eslint-disable no-unused-vars */
-    constructor(...args) {
-        super()
-    }
-}
